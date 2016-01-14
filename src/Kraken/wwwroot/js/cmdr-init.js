0f1d66261063f9f9c8344c41f4f394566b03d226
John Cruikshank <cruikshj@live.com>
2016-01-14 02:59:44 -0600

    cmdrjs update

--- src/Kraken/wwwroot/js/cmdr-init.js
+++ src/Kraken/wwwroot/js/cmdr-init.js
@@ -1,27 +1,15 @@
 ﻿(function(app, cmdr, $) {
 
     var shell = new cmdr.OverlayShell();
-
-    function define(definition) {
-        shell.definitionProvider.addDefinition(new cmdr.Definition(definition));
-    }
-
-    define({
-        name: 'batch',
-        description: 'Lists all release batches',
-        callback: function() {
+    
+    shell.define({
+        name: 'BATCH',
+        description: 'Lists all release batches.',
+        main: function() {
             return $.get(app.basePath + 'api/releasebatches').then(function(data) {
-                this.shell.writeLine("Project batches");
+                this.shell.writeLine("Project batches:");
                 this.shell.writeLine();
-                this.shell.writePad('id', ' ', 10);
-                this.shell.writeLine('name');
-                this.shell.writePad('--', ' ', 10);
-                this.shell.writeLine('----');
-                $.each(data, function(index, item) {
-                    this.shell.writePad(item.id.toString(), ' ', 10);
-                    this.shell.writeLine(item.name);
-                }.bind(this));
-                this.shell.writeLine();
+                this.shell.writeTable(data, ['id:10', 'name:*'], true);
             }.bind(this)).fail(function(xhr, error, message) {
                 this.shell.writeLine(message, 'error');
                 this.shell.writeLine('Operation failed', 'error');
@@ -29,10 +17,11 @@
         }
     });
 
-    define({
-        name: 'mkbatch',
-        description: 'Creates a new release batch (e.g. mkbatch "My Batch")',
-        callback: function(name) {
+    shell.define({
+        name: 'MKBATCH',
+        description: 'Creates a new release batch.',
+        usage: 'MKBATCH name',
+        main: function (name) {
             if (!name) {
                 this.shell.writeLine('Project batch name required', 'error');
                 return;
@@ -52,10 +41,11 @@
         }
     });
 
-    define({
-        name: 'renbatch',
-        description: 'Renames a release batch (e.g. renbatch 123 "My Other Batch")',
-        callback: function (id, name) {
+    shell.define({
+        name: 'RENBATCH',
+        description: 'Renames a release batch.',
+        usage: 'RENBATCH id name',
+        main: function (id, name) {
             if (!id || !name) {
                 this.shell.writeLine('Project batch id and name required', 'error');
                 return;
@@ -63,7 +53,7 @@
             return $.ajax({
                 type: 'PUT',
                 url: app.basePath + 'api/releasebatches/' + id,
-                data: JSON.stringify({ name: name }),
+                data: JSON.stringify({ id: id, name: name }),
                 contentType: 'application/json'
             }).then(function () {
                 $(document).trigger('releasebatches.update', id);
@@ -75,15 +65,16 @@
         }
     });
 
-    define({
-        name: 'rmbatch',
-        description: 'Deletes a release batch by id (e.g. rmbatch 123)',
-        callback: function(id) {
+    shell.define({
+        name: 'RMBATCH',
+        description: 'Deletes a release batch by id.',
+        usage: 'RMBATCH id',
+        main: function (id) {
             if (!id) {
                 this.shell.writeLine('Project batch id required', 'error');
                 return;
             }
-            var deferred = $.Deferred();
+            var deferred = this.defer();
             this.shell.write('Are you sure you want to delete this release batch (y/n)? ', 'warning');
             this.shell.readLine(function(value) {
                 if (value.toLowerCase() !== 'y') {
@@ -102,26 +93,18 @@
                     deferred.resolve();
                 }.bind(this));
             }.bind(this));
-            return deferred.promise();
+            return deferred;
         }
     });
 
-    define({
-        name: 'env',
-        description: 'Lists all Octopus environments',
-        callback: function() {
+    shell.define({
+        name: 'ENV',
+        description: 'Lists all Octopus environments.',
+        main: function () {
             return $.get(app.basePath + 'api/environments').then(function(data) {
-                this.shell.writeLine('Octopus environments');
+                this.shell.writeLine('Octopus environments:');
                 this.shell.writeLine();
-                this.shell.writePad('id', ' ', 20);
-                this.shell.writeLine('name');
-                this.shell.writePad('--', ' ', 20);
-                this.shell.writeLine('----');
-                $.each(data, function(index, item) {
-                    this.shell.writePad(item.id.toString(), ' ', 20);
-                    this.shell.writeLine(item.name);
-                }.bind(this));
-                this.shell.writeLine();
+                this.shell.writeTable(data, ['id:20', 'name:*'], true);
             }.bind(this)).fail(function(xhr, error, message) {
                 this.shell.writeLine(message, 'error');
                 this.shell.writeLine('Operation failed', 'error');
@@ -129,11 +112,11 @@
         }
     });
 
-    define({
-        name: 'logout',
-        description: 'Logout of Kraken',
-        callback: function() {
-            var deferred = $.Deferred();
+    shell.define({
+        name: 'LOGOUT',
+        description: 'Logout of Kraken.',
+        main: function () {
+            var deferred = this.defer();
             this.shell.write('Are you sure you want to logout of Kraken? ', 'warning');
             this.shell.readLine(function(value) {
                 if (value.toLowerCase() !== 'y') {
@@ -142,8 +125,7 @@
                 document.location = app.basePath + 'logout';
                 deferred.resolve();
             }.bind(this));
-
-            return deferred.promise();
+            return deferred;
         }
     });
 
