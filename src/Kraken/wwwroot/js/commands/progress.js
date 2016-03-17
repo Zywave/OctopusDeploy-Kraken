fbc94f54b022599571bafac4227c55768669bfcb
John Cruikshank <cruikshj@live.com>
2016-03-17 14:01:10 -0500

    feat: progress command cleanup

--- src/Kraken/wwwroot/js/commands/progress.js
+++ src/Kraken/wwwroot/js/commands/progress.js
@@ -1,22 +1,23 @@
-﻿define(['cmdr', 'services/releaseBatches', 'underscore'], function (cmdr, releaseBatchesService, _) {
+﻿define(['cmdr', 'services/releaseBatches'], function (cmdr, releaseBatchesService) {
 
     return new cmdr.Definition({
         name: 'PROGRESS',
         description: 'Gets the deplyoment progress from Octopus for all projects in a release batch.',
-        usage: 'PROGRESS name',
-        main: function (name) {
-            if (!name) {
-                this.shell.writeLine('Project batch name required', 'error');
+        usage: 'PROGRESS idOrName',
+        main: function (idOrName) {
+            if (!idOrName) {
+                this.shell.writeLine('Project batch id or name required', 'error');
                 return;
             }
 
-            return releaseBatchesService.getProgression(name).then(function (data) {
-                this.shell.writeLine('Progression for batch idOrName ' + name);
+            return releaseBatchesService.getProgression(idOrName).then(function (data) {
+                this.shell.writeLine('Progression for batch ' + idOrName);
                 this.shell.writeLine();
                 this.shell.writeTable(data, ['projectId:20:project id', 'environmentId:20:environment', 'releaseVersion:20:release version', 'state:*:state'], true);
                 return data;
             }.bind(this)).fail(this.fail.bind(this));
-        }
+        },
+        autocompleteKeys: ['releaseBatches']
     });
 
 });
